58f9fed1762fe08b9336a03757dfb4f786615d22
Beatriz <beamanga@gmail.com>
2019-02-13 12:19:06 +0100

    Routing realizado en header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,15 @@ import { HeaderComponentComponent } from './header-component/header-component.co
 import { FooterComponentComponent } from './footer-component/footer-component.component';
 import { IndividualComponentComponent } from './individual-component/individual-component.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes=[
+  {path:'full-list', component:ListaComponent},
+  {path:'one-to-one', component:IndividualComponentComponent},
+  {path:'new', component:FormAltaComponent},
+  {path:'delete', component:FormBajaComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,7 +34,11 @@ import { IndividualComponentComponent } from './individual-component/individual-
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [PortfoliosService],
   bootstrap: [AppComponent]
