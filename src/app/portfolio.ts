29469a969a73a307eb9e3f3cc20230a0518baa63
Beatriz <beamanga@gmail.com>
2019-02-14 12:56:56 +0100

    Alta Portfolio

--- src/app/portfolio.ts
+++ src/app/portfolio.ts
@@ -1,33 +1,16 @@
 export class Portfolio {
-    constructor(
-        private nombre: string,
-        private descripcion: string,
-        private fichero: string
-    ){
-        if(this.nombre==""){
-            console.error("Nombre no puede estar vacío");
-            throw new Error("Nombre no puede estar vacío");
-        }
-    }
-    //getters and setters
-    public getNombre():string{
-        return this.nombre;
-    }
-    public setNombre(nombre:string):void{
-        this.nombre=nombre;
-    }
-
-    public getDescripcion():string{
-        return this.descripcion;
-    }
-    public setDescripcion(descripcion:string):void{
-        this.descripcion=descripcion;
-    }
-
-    public getFichero():string{
-        return this.fichero;
-    }
-    public setFichero(fichero:string):void{
-        this.fichero=fichero;
+    public nombre: string;
+    public descripcion: string;
+    public fichero: string;
+    constructor(nombre:string, descripcion:string, fichero:string){
+        
+            if(nombre==""){
+                console.error("Nombre no puede estar vacío");
+                throw new Error("Nombre no puede estar vacío");
+            }
+            this.nombre=nombre;
+            this.descripcion=descripcion;
+            this.fichero=fichero;
+        
     }
 }
